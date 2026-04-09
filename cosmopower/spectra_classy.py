918332839c248159c09cce8dd20caaf9143c6e97
Hidde Jense <JenseH@cardiff.ac.uk>
2026-04-09 11:45:19 +0100

    Added linear matter power spectrum.

--- cosmopower/spectra_classy.py
+++ cosmopower/spectra_classy.py
@@ -31,6 +31,7 @@ def classy_args_interpret(parser: YAMLParser, extra_args: dict = {}) -> dict:
     extra_args["lensing"] = "no"
     cl_spec = set(extra_args.get("output", "").split(" "))
     cl_modes = set(extra_args.get("modes", "s").split(" "))
+    want_cl = False
 
     for quantity in parser.quantities:
         if quantity == "derived":
@@ -39,6 +40,7 @@ def classy_args_interpret(parser: YAMLParser, extra_args: dict = {}) -> dict:
 
         qpath = quantity.split("/")
         if qpath[0] == "Cl" or qpath[0] == "unlensed_Cl":
+            want_cl = True
             if qpath[1] == "tt" or qpath[1] == "te":
                 cl_spec.add("tCl")
             if qpath[1] == "te" or qpath[1] == "ee":
@@ -53,10 +55,23 @@ def classy_args_interpret(parser: YAMLParser, extra_args: dict = {}) -> dict:
             if "t" in cl_modes:
                 extra_args["l_max_tensors"] = max(extra_args["l_max_tensors"],
                                                   parser.modes(quantity).max())
+        elif qpath[0] == "Pk":
+            cl_spec.add("mPk")
+            zval = parser.parameter_value("z")
+            if type(zval) == float:
+                z_max_pk = zval
+            elif type(zval) == tuple:
+                z_max_pk = max(zval)
+            extra_args["z_max_pk"] = max(extra_args.get("z_max_pk", 0.0), z_max_pk)
+            k_max_pk = parser.modes(quantity).max()
+            extra_args["P_k_max_1/Mpc"] = max(extra_args.get("P_k_max_1/Mpc", 1.0), k_max_pk)
 
-    if "s" not in cl_modes:
+            if qpath[1] == "nonlin":
+                extra_args["non_linear"] = "yes"
+
+    if "s" not in cl_modes or not want_cl:
         extra_args.pop("l_max_scalars")
-    if "t" not in cl_modes:
+    if "t" not in cl_modes or not want_cl:
         extra_args.pop("l_max_tensors")
     extra_args["output"] = " ".join(list(cl_spec))
     extra_args["modes"] = " ".join(list(cl_modes))
@@ -85,6 +100,9 @@ def get_spectra(parser: YAMLParser, state: dict, args: dict = {},
                 quantities: list = [], extra_args: dict = {}) -> bool:
 
     classy = state["module"]
+    
+    if "z" in args:
+        z_pk = args.pop("z")
 
     try:
         cosmo = classy.Class()
@@ -117,8 +135,16 @@ def get_spectra(parser: YAMLParser, state: dict, args: dict = {},
             ell = cls["ell"]
             Cl = cls[spec]
             Dl = Cl * (ell * (ell + 1) / (2 * np.pi))
+        elif qpath[0] == "Pk":
+            k = state[quantity + ".modes"]
+            spec = qpath[1]
+            
+            if spec == "lin":
+                Pk = np.zeros_like(k)
+                for i in range(len(k)):
+                    Pk[i] = cosmo.pk_lin(k[i], z_pk)
 
-            state[quantity] = Dl[state[quantity + ".modes"]]
+            state[quantity] = Pk
 
     cosmo.struct_cleanup()
     cosmo.empty()
